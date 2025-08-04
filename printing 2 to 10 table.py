d6300c31b8fd848cde25e2c5dba7494b6115f3f0
ni783 <nishumishra1947@gmail.com>
2025-08-04 10:32:06 +0530

    Add files via upload

---
+++ printing 2 to 10 table.py
@@ -0,0 +1,43 @@
+# Table of 2
+for n in range (1,11):
+    print("2 x",n,"=",2*n)
+print("\n")
+
+# Table of 3
+for n in range (1,11):
+    print("3 x",n,"=",3*n)
+print("\n")
+
+# Table of 4
+for n in range (1,11):
+    print("4 x",n,"=",4*n)    
+print("\n")
+
+# Table of 5
+for n in range (1,11):
+    print("5 x",n,"=",5*n) 
+print("\n")
+
+# Table of 6
+for n in range (1,11):
+    print("6 x",n,"=",6*n)
+print("\n")
+
+# Table of 7
+for n in range (1,11):
+    print("7 x",n,"=",7*n)
+print("\n")
+
+# Table of 8
+for n in range (1,11):
+    print("8 x",n,"=",8*n)
+print("\n")
+
+# Table of 9
+for n in range (1,11):
+    print("9 x",n,"=",9*n)
+print("\n")
+
+# Table of 10
+for n in range (1,11):
+    print("10 x",n,"=",10*n)
